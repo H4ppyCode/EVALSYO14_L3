d0d2323fd28927e8e4d8439767090bca9c91b67c
pdhiser@la-joliverie.com <pdhiser@la-joliverie.com>
2022-12-23 10:38:21 +0100

    v3

--- src/Dico.py
+++ src/Dico.py
@@ -28,30 +28,29 @@ import random
 import socket
 import urllib
 import urllib.request
+import socket
+from random import choice
 
-
-HOST, PORT = "localhost", 5000
+# Adresse et port du middleware
+MIDDLEWARE_ADDRESS = ("localhost", 5001)
 
 # Créez un socket en mode UDP
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Liez le socket à l'adresse et au port donnés
-sock.bind((HOST, PORT))
-
-# Créez une liste de mots aléatoires
-words = []
-with urllib.request.urlopen("https://karczmarczuk.users.greyc.fr/TEACH/TAL/Textes/motsfrgut.txt") as f:
-    words = f.read().decode(encoding="iso-8859-1").split()
+# Liez le socket à l'adresse et au port du serveur
+server_sock.bind(("localhost", 5000))
 
 while True:
-    # Recevez des données envoyées par le client
-    data, addr = sock.recvfrom(1024)
+    # Recevez des données envoyées par le middleware
+    data, addr = server_sock.recvfrom(1024)
     data = data.decode()
-    print("Données reçues de {} : {}".format(addr, data))
+    print("Données reçues du middleware {} : {}".format(addr, data))
 
-    # Si le client envoie la commande "start game", envoyez un mot aléatoire au client
+    # Si la commande est "start game", envoyez un mot aléatoire au client
     if data == "start game":
-        word = random.choice(words)
-        sock.sendto(word.encode(), addr)
+        with urllib.request.urlopen("https://karczmarczuk.users.greyc.fr/TEACH/TAL/Textes/motsfrgut.txt") as f:
+            words = f.read().decode(encoding="iso-8859-1").split()
 
-
+    word = random.choice(words)
+    server_sock.sendto(word.encode(), MIDDLEWARE_ADDRESS)
+    print("Mot aléatoire envoyé au client : {}".format(word))
--- src/Middleware.py
+++ src/Middleware.py
@@ -1,1 +1,44 @@
-print('hello')
+import socket
+
+# Adresse et port du serveur
+SERVER_ADDRESS = ("localhost", 5000)
+
+CLIENT_ADDRESS = ("localhost", 5002)
+
+# Créez un socket en mode UDP
+middleware_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Liez le socket à l'adresse et au port du middleware
+middleware_sock.bind(("localhost", 5001))
+
+while True:
+    # Recevez des données envoyées par le client
+    data, addr = middleware_sock.recvfrom(1024)
+    data = data.decode()
+    print("Données reçues du client {} : {}".format(addr, data))
+
+    # Envoyez les données au serveur
+    middleware_sock.sendto(data.encode(), SERVER_ADDRESS)
+
+    # Recevez une réponse du serveur
+    data, addr = middleware_sock.recvfrom(1024)
+    data = data.decode()
+    print("Données reçues du serveur {} : {}".format(addr, data))
+    # Recevez une réponse du serveur
+    data, addr = middleware_sock.recvfrom(1024)
+
+    data = data.decode()
+    print("Données reçues du serveur {} : {}".format(addr, data))
+    # Envoyez la réponse au client
+    middleware_sock.sendto(data.encode(), addr)
+
+    # Envoyer ok au client
+    data = "ok"
+    print("Données envoyé au Client {} : {}".format(addr, data))
+    middleware_sock.sendto(data.encode(), SERVER_ADDRESS)
+
+
+    data, addr = "check moi ça"
+    data = data.decode()
+    middleware_sock.sendto(data.encode(), SERVER_ADDRESS)
+    print('okay')
